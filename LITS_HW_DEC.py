3c4501f4fd576a0c5afb24c82f7c70aa7f81c294
Serge Chumachenko <chumachenko.sergiy@gmail.com>
2017-02-04 10:32:00 +0200

    Init

---
+++ LITS_HW_DEC.py
@@ -0,0 +1,45 @@
+from decimal import*
+class My_Zero_Division_Error(Exception):
+    pass
+class My_Type_Error(Exception):
+    pass
+
+def type_check(arg_type):
+    def my_func(func):
+        def wrapper(*args):
+            if all([isinstance(arg,arg_type)for arg in args]):
+                res = func(*args)
+                return res
+            else:
+                raise My_Type_Error
+        return wrapper
+    return my_func
+
+def my_dec4zero(func):
+    def wrapper(x,y):
+        if y == 0:
+            raise My_Zero_Division_Error
+        return func(x,y)
+    return wrapper
+
+@type_check((int,float,Decimal))
+def summ(x,y):
+    return x + y
+
+@type_check((int,float,Decimal))
+def sub(x,y):
+    return x - y
+
+@type_check((int,float,Decimal))
+def mul(x,y):
+    return x * y
+
+@type_check((int,float,Decimal))
+@my_dec4zero
+def div(x,y):
+    return x / y
+
+print(summ(547.54327,451.45673))
+print(sub(2.477,1.478))
+print(mul(Decimal(2.5),Decimal(2.5)))
+print(div(Decimal(5),Decimal(5.5)))
